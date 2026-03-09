39719fa54cbd1f2ebf67598a2f1dbd716329d739
Kaviarasan E <144410585+Ekaviyarasan@users.noreply.github.com>
2026-03-10 01:18:01 +0530

    up

---
+++ Assienments/React/game/vite/com/my/src/component/Student.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Detail from './Detail'
+
+function Student() {
+const high=[{
+    name:"kavi",
+    rank:1
+}]
+
+  return (
+    <>
+    <Detail  top={high} />
+    </>
+  )
+}
+
+export default Student
